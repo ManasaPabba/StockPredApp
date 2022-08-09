bf797b02b2eb83af0f2b218f530ba98e97ef8c2e
ManasaPabba <110091594+ManasaPabba@users.noreply.github.com>
2022-08-09 19:39:06 +0530

    Update start.sh

--- postgres/start.sh
+++ postgres/start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# You could probably do this fancier and have an array of extensions
-# to create, but this is mostly an illustration of what can be done
+
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<EOF
 CREATE EXTENSION plpython3u;
